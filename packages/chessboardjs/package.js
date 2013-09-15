0b6c28ac45d2caaeeff69ef5d7f4ad3304aca1a2
Cheeseen <aychtang@gmail.com>
2013-09-15 16:42:07 +0100

    Working images.

--- packages/chessboardjs/package.js
+++ packages/chessboardjs/package.js
@@ -6,17 +6,4 @@ Package.describe({
 Package.on_use(function (api) {
   api.add_files('js/chessboard-0.3.0.min.js', ['client']);
   api.add_files('css/chessboard-0.3.0.min.css', ['client']);
-
-  api.add_files('img/chesspieces/wikipedia/bB.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/bK.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/bN.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/bP.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/bQ.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/bR.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/wB.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/wK.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/wN.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/wP.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/wQ.png', ['client']);
-  api.add_files('img/chesspieces/wikipedia/wR.png', ['client']);
 });
