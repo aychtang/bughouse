1595768eaba61e3cc1bf0655a9884ef44d1856a9
Cheeseen <aychtang@gmail.com>
2013-09-15 16:47:22 +0100

    Outputs object model of pieces on board, onchange.

--- client/bughouse.js
+++ client/bughouse.js
@@ -1,7 +1,15 @@
 
 Meteor.startup(function() {
+	var onChange = function(oldPos, newPos) {
+	  console.log("Position changed:");
+	  console.log("Old position: " , ChessBoard.fenToObj(ChessBoard.objToFen(oldPos)));
+	  console.log("New position: " , ChessBoard.fenToObj(ChessBoard.objToFen(newPos)));
+	  console.log("--------------------");
+	};
+
 	var board1 = new ChessBoard('board1', {
 		draggable: true,
-		position: 'start'
+		position: 'start',
+		onChange: onChange
 	});
 });
